4ff0986b0d90ef8dcee35aaa5b545ce80b4e40c6
Fang <fang.live-@msn.com>
2018-01-20 04:50:07 +0700

    add ui works

---
+++ src/component/CategoryWorks.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../css/categoryWorks.css';
+
+class CategoryWorks extends React.Component{
+    render(){
+        return()
+    }
+}
+
+export default CategoryWorks
---
+++ src/pages/Findworks.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../css/findworks.css';
+
+
+
+class Findworks extends React.Component{
+    render(){
+        return(
+
+        )
+    }
+}
+
+export default Findworks
